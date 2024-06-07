6af756380f240e0e733a35c18421789e623240b1
Yuhan Wang <62628137+yhanw0719@users.noreply.github.com>
2024-06-07 16:13:02 -0700

    update process_emission.R to v2
    read two separate emission inputs, one for brake and tire wear (btw) onroad LDV emissions, and the other for non-btw onroad LDV emissions

--- process_emission.R
+++ process_emission.R
@@ -1,20 +1,21 @@
 #!/usr/bin/Rscript
 
 ## read emission input
-em_input = read.csv(file.path(inputdir,emis_inputfname))
-em_input$sector = tolower(em_input$sector)
-
-## filter by category
-em = filter(em_input, sector==tolower(emis_category))
+em_btw = read.csv(file.path(inputdir,emis_inputfname_btw)) %>% rename('isrm'='ISRM')
+em_nbtw = read.csv(file.path(inputdir,emis_inputfname_nbtw)) %>% rename('isrm'='ISRM')
 
 ## check emission input 
-if (dim(em)[1]==0){
-  print('ERROR: No emission data - Check emission input or category')
+if (dim(em_btw)[1]==0){
+  print('ERROR: No brake-and-tire-wear emission data - Check emission input or category')
+} 
+if (dim(em_nbtw)[1]==0){
+  print('ERROR: No non-brake-and-tire-wear emission data - Check emission input or category')
 } 
 
 ## end of script
 print('---------------')
-print('ISRM-level NEI emission input and category:')
-print(emis_inputfname)
-print(emis_category)
+print('ISRM-level NEI emission inputs:')
+print(em_input_btw)
+print(em_input_nbtw)
+
 
